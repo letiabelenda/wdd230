9fd356e05da268c115b49e06a6e1a7064c57d5db
letiabelenda <l_abelenda@hotmail.com>
2023-09-25 17:18:35 -0300

    Hamburger Menu done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -15,3 +15,12 @@ const secondsModified = lastModified.getSeconds();
 
 const completeDateModified = `Last Modification: ${dayModified}/${monthModified}/${yeartModified} ${hourModified}:${minutesModified}:${secondsModified}`;
 document.getElementById('lastModified').textContent = completeDateModified;
+
+// Hamburger Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
