01d8f4db9922da564ed2c1e1ea0bbc50d9bc061d
letiabelenda <l_abelenda@hotmail.com>
2023-10-03 16:33:20 -0300

    03102023

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -17,10 +17,10 @@ const completeDateModified = `Last Modification: ${dayModified}/${monthModified}
 document.getElementById('lastModified').textContent = completeDateModified;
 
 // Hamburger Menu
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
+function toggleMenu() {
+    document.getElementById("navigationLinks").classList.toggle("open")
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+ }
+ 
+ const x = document.getElementById("hamburgerBtn")
+ x.onclick = toggleMenu;
