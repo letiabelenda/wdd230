1ccc0b74c125f7971aeb9b1a9ed6380e05a0cd12
letiabelenda <l_abelenda@hotmail.com>
2023-10-10 16:37:36 -0300

    bom code done

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -2,24 +2,37 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+// Store added chapters
+const addedItems = new Set();
+
 button.addEventListener('click', () => {
-    if (input.value != '') {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
+    if (input.value !== '') {
+        // Check if the item is already added
+        if (!addedItems.has(input.value)) {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
 
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        
-        li.append(deleteButton);
-        list.append(li);
+            li.textContent = input.value;
+            deleteButton.textContent = '❌';
 
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
+            li.append(deleteButton);
+            list.append(li);
+
+            deleteButton.addEventListener('click', () => {
+                list.removeChild(li);
+                input.focus();
+                // Remove the chapter from the set
+                addedItems.delete(input.value);
+            });
+
+            // Add chapter to the set
+            addedItems.add(input.value);
+
             input.focus();
-        });
-
-        input.focus();
-        input.value = '';;
+            input.value = '';
+        } else {
+            window.alert('Chapter already added! ⚠️');
+        }
     } else {
         window.alert('Please, write something! 👇');
     }
