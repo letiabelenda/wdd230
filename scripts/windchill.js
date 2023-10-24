ed18885f59f8b534fe574d2a98ab8d76c24f9ec6
letiabelenda <l_abelenda@hotmail.com>
2023-10-24 14:27:26 -0300

    24102023

---
+++ scripts/windchill.js
@@ -0,0 +1,28 @@
+
+
+  const temperatureElement = document.querySelector(".temperature");
+  const windSpeedElement = document.querySelector(".wind-speed");
+  const windChillElement = document.querySelector(".wind-chill");
+  
+  const currentTemperature = parseFloat(temperatureElement.textContent);
+  const currentWindSpeed = parseFloat(windSpeedElement.textContent);
+  
+  // Calculate wind chill
+  const windChill = calculateWindChill(currentTemperature, currentWindSpeed);
+  
+  // Display temperature, wind speed and wind chill values in the weather section
+  temperatureElement.textContent = `Temperature: ${currentTemperature}°F`;
+  windSpeedElement.textContent = `Wind Speed: ${currentWindSpeed} mph`;
+  windChillElement.textContent = `Wind Chill: ${windChill}`;
+
+  // Function to calculate wind chill
+function calculateWindChill(temperature, windSpeed) {
+    // Check if temperature and wind speed meet the requirements
+    if (temperature <= 50 && windSpeed > 3.0) {
+      // Calculate wind chill using the formula
+      const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+      return Math.round(windChill) + "°F";
+    } else {
+      return "N/A";
+    }
+  }
