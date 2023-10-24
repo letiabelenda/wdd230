692db2154f16deb12478f788e791ea8eebc68c1b
letiabelenda <l_abelenda@hotmail.com>
2023-10-24 16:53:18 -0300

    css base and large discover page done

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -24,3 +24,24 @@ function toggleMenu() {
  
  const x = document.getElementById("hamburgerBtn")
  x.onclick = toggleMenu;
+
+
+ // Number of visits
+ const visitsDisplay = document.querySelector("#visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit! 🎉`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+
