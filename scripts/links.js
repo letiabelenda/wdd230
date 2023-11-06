d30a4364775c8b5e3710bad0f219cc698d1dd082
letiabelenda <l_abelenda@hotmail.com>
2023-11-06 17:38:28 -0300

    06112023

---
+++ scripts/links.js
@@ -0,0 +1,38 @@
+const baseURL = "https://letiabelenda.github.io/wdd230/";
+const linksURL = "https://letiabelenda.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+
+function displayLinks(weeks) {
+  const lessonList = document.getElementById("container");
+
+  weeks.lessons.forEach(week => {
+    const weekElement = document.createElement("div");
+    weekElement.className = "week";
+
+    const weekTitle = document.createElement("h4");
+    weekTitle.textContent = "Week " + week.lesson;
+
+    const linksList = document.createElement("ul");
+
+    week.links.forEach(link => {
+      const linkItem = document.createElement("li");
+      const linkAnchor = document.createElement("a");
+      linkAnchor.href = baseURL + link.url;
+      linkAnchor.textContent = link.title;
+
+      linkItem.appendChild(linkAnchor);
+      linksList.appendChild(linkItem);
+    });
+
+    weekElement.appendChild(weekTitle);
+    weekElement.appendChild(linksList);
+    lessonList.appendChild(weekElement);
+  });
+}
+
+getLinks();
