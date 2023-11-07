2d0077d9c13595b9562c5f2d2d3ed0490261741b
letiabelenda <l_abelenda@hotmail.com>
2023-11-07 13:50:13 -0300

    almost done

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,5 +1,5 @@
 const url = 'https://letiabelenda.github.io/wdd230/chamber/data/members.json';
-const cards = document.querySelector('cards');
+const cards = document.querySelector('.cards');
 
 async function getMembersData() {
     const response = await fetch(url);
@@ -10,42 +10,44 @@ async function getMembersData() {
 getMembersData();
 
 function displayMembers(members) {
-    //Create card elements
-    let card = document.createElement('section');
-    let h2 = document.createElement('h3');
-    let portrait = document.createElement('img');4
-    let adress = document.createElement('p');
-    let phone = document.createElement('p');
-    let membership = document.createElement('p');
-    let website = document.createElement('a');
-    let email = document.createElement('p');
+    members.forEach(member => {
+        //Create card elements
+        let card = document.createElement('section');
+        let h2 = document.createElement('h3');
+        let portrait = document.createElement('img');4
+        let adress = document.createElement('p');
+        let phone = document.createElement('p');
+        let membership = document.createElement('p');
+        let website = document.createElement('a');
+        let email = document.createElement('p');
 
-    // Company name
-    h2.textContent = members.name;
+        // Company name
+        h2.textContent = member.name;
 
-    //Image
-    portrait.setAttribute('src', members.image);
-    portrait.setAttribute('alt', 'Image of' + members.name + 'company');
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('height', '250px');
+        //Image
+        portrait.setAttribute('src', member.image);
+        portrait.setAttribute('alt', 'Image of ' + member.name + ' company');
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('height', '250px');
 
-    //Information
-    adress.textContent = members.adress;
-    phone.textContent = members.phone;
-    website.textContent = members.website;
-    membership.textContent = members.membership;
-    email.textContent = members.other-info;
+        //Information
+        adress.textContent = member.adress;
+        phone.textContent = member.phone;
+        website.textContent = member.website;
+        website.setAttribute('href', member.website)
+        membership.textContent = member.membership;
+        email.textContent = member['other-info'];
 
-    //Add the card with the elements
-    card.appendChild(h2);
-    card.appendChild(portrait);
-    card.appendChild(adress);
-    card.appendChild(phone);
-    card.appendChild(membership);
-    card.appendChild(website);
-    card.appendChild(email);
-
-    document.querySelector('article.cards').appendChild(card);
+        //Add the card with the elements
+        card.appendChild(h2);
+        card.appendChild(portrait);
+        card.appendChild(adress);
+        card.appendChild(phone);
+        card.appendChild(membership);
+        card.appendChild(website);
+        card.appendChild(email);
+        cards.appendChild(card);
+    });
 }
 
 // Grid and List buttons
