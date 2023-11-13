2188c118ca628c05072f9649346b09c23815820a
letiabelenda <l_abelenda@hotmail.com>
2023-11-13 12:03:06 -0300

    13112023

---
+++ scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Montevideo&units=imperial&appid=0a65e95b7d2094ce2cd33a12b03b9c98';
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else{
+            throw Error(await response.text());
+        }
+
+    } catch (error){
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data){
+    const desc = data.weather[0].description.toUpperCase();
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+    currentTemp.innerHTML = `<p>Current Temperature: ${data.main.temp.toFixed(0)} °F </p>`;
+    captionDesc.innerHTML = `<p>Description: ${description}</p>`;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+}
+
