0e22729e15595152471cd38fd3632d20f98d0f3f
letiabelenda <l_abelenda@hotmail.com>
2023-11-17 14:29:48 -0300

    json file modified

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,43 +10,68 @@ async function getMembersData() {
 getMembersData();
 
 function displayMembers(members) {
+    const display = document.querySelector('.cards');
+    display.innerHTML = '';
+
     members.forEach(member => {
-        //Create card elements
-        let card = document.createElement('section');
-        let h2 = document.createElement('h3');
-        let portrait = document.createElement('img');4
-        let adress = document.createElement('p');
-        let phone = document.createElement('p');
-        let membership = document.createElement('p');
-        let website = document.createElement('a');
-        let email = document.createElement('p');
+        let card;
+        if (display.classList.contains('grid')) {
+            // Create card elements for grid view
+            card = document.createElement('div');
+            card.classList.add('card-grid');
 
-        // Company name
-        h2.textContent = member.name;
+            let h2 = document.createElement('h3');
+            let portrait = document.createElement('img');
+            let adress = document.createElement('p');
+            let phone = document.createElement('p');
+            let membership = document.createElement('p');
+            let website = document.createElement('a');
+            let email = document.createElement('p');
 
-        //Image
-        portrait.setAttribute('src', member.image);
-        portrait.setAttribute('alt', 'Image of ' + member.name + ' company');
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('height', '250px');
+            h2.textContent = member.name;
+            portrait.setAttribute('src', member.image);
+            portrait.setAttribute('alt', 'Image of ' + member.name + ' company');
+            portrait.setAttribute('loading', 'lazy');
+            portrait.setAttribute('height', '250px');
 
-        //Information
-        adress.textContent = member.adress;
-        phone.textContent = member.phone;
-        website.textContent = member.website;
-        website.setAttribute('href', member.website)
-        membership.textContent = member.membership;
-        email.textContent = member['other-info'];
+            card.appendChild(h2);
+            card.appendChild(portrait);
+            card.appendChild(adress);
+            card.appendChild(phone);
+            card.appendChild(membership);
+            card.appendChild(website);
+            card.appendChild(email);
+            cards.appendChild(card);
+        } else {
+            // Create card elements for list view
+            card = document.createElement('section');
+            card.classList.add('card-list');
 
-        //Add the card with the elements
-        card.appendChild(h2);
-        card.appendChild(portrait);
-        card.appendChild(adress);
-        card.appendChild(phone);
-        card.appendChild(membership);
-        card.appendChild(website);
-        card.appendChild(email);
-        cards.appendChild(card);
+            let h2 = document.createElement('h3');
+            let address = document.createElement('p');
+            let phone = document.createElement('p');
+            let membership = document.createElement('p');
+            let website = document.createElement('a');
+            let email = document.createElement('p');
+
+            h2.textContent = member.name;
+            address.textContent = `Address: ${member.adress}`;
+            phone.textContent = `Phone: ${member.phone}`;
+            website.textContent = member.website;
+            website.setAttribute('href', member.website)
+            membership.textContent = member.membership;
+            email.textContent = member['other-info'];
+
+            card.appendChild(h2);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(membership);
+            card.appendChild(website);
+            card.appendChild(email);
+            cards.appendChild(card);
+        }
+
+        display.appendChild(card);
     });
 }
 
