cf3484ac454177cb50665f32ff654b1df278b143
letiabelenda <l_abelenda@hotmail.com>
2023-11-27 16:09:56 -0300

    rentals page almost done

--- final-project/scripts/script.js
+++ final-project/scripts/script.js
@@ -17,45 +17,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Rentals table
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('data/rentals.json')
-      .then(response => response.json())
-      .then(data => {
-        const rentalsTable = document.getElementById('rentalsTable');
-  
-        data.rentals.forEach(rental => {
-          const row = document.createElement('tr');
-          row.innerHTML = `
-            <td>${rental.rentalType}</td>
-            <td>${rental.maxPersons}</td>
-            <td>$${rental.halfDayPrice.walkIn}</td>
-            <td>$${rental.fullDayPrice.reservation}</td>
-          `;
-          rentalsTable.appendChild(row);
-        });
-      })
-      .catch(error => {
-        console.log('Error fetching data:', error);
+  fetch('data/rentals.json')
+    .then(response => response.json())
+    .then(data => {
+      const rentalsTable = document.getElementById('rentalsTable');
+
+      data.rentals.forEach(rental => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${rental["Rental Type"]}</td>
+          <td>${rental["Max. Persons"]}</td>
+          <td>${rental.Reservation["Half Day"]}</td>
+          <td>${rental.Reservation["Full Day"]}</td>
+          <td>${rental["Walk-In"]["Half Day"]}</td>
+          <td>${rental["Walk-In"]["Full Day"]}</td>
+        `;
+        rentalsTable.appendChild(row);
       });
-  });
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const reservationForm = document.getElementById('reservationForm');
-  
-    reservationForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const date = document.getElementById('rentalDate').value;
-      const period = document.getElementById('rentalPeriod').value;
-      const rentalType = document.getElementById('rentalType').value;
-      const numberOfRentals = document.getElementById('numberOfRentals').value;
-      const phoneNumber = document.getElementById('phoneNumber').value;
-      const fullName = document.getElementById('fullName').value;
-      const email = document.getElementById('email').value;
-      const homeLocation = document.getElementById('homeLocation').value;
-      const cruiseLine = document.getElementById('cruiseLine').value;
-      const specialAccommodations = document.getElementById('specialAccommodations').value;
-
-      window.location.href = "gracias.html";
+    })
+    .catch(error => {
+      console.log('Error fetching data:', error);
     });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const reservationForm = document.getElementById('reservationForm');
+  
+  reservationForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const date = document.getElementById('rentalDate').value;
+    const period = document.getElementById('rentalPeriod').value;
+    const rentalType = document.getElementById('rentalType').value;
+    const numberOfRentals = document.getElementById('numberOfRentals').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const fullName = document.getElementById('fullName').value;
+    const email = document.getElementById('email').value;
+    const homeLocation = document.getElementById('homeLocation').value;
+    const cruiseLine = document.getElementById('cruiseLine').value;
+    const specialAccommodations = document.getElementById('specialAccommodations').value;
+
+    window.location.href = "gracias.html";
+  });
 });
